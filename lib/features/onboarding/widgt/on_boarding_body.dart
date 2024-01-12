995fb4d75ef1150720ce9d9e8ec9278bf7e631c9
mohamadgazer <mohamadgazer@yahoo.com>
2024-01-13 01:42:50 +0200

    add onboarding image preper onboarding files

--- lib/features/onboarding/widgt/on_boarding_body.dart
+++ lib/features/onboarding/widgt/on_boarding_body.dart
@@ -1,48 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:installment/features/onboarding/data/onboarding_local_data.dart';
 
-import 'on_boarding_page_view_item.dart';
-
-class OnBoardingBody extends StatefulWidget {
+class OnBoardingBody extends StatelessWidget {
   const OnBoardingBody({
     super.key,
   });
 
   @override
-  State<OnBoardingBody> createState() => _OnBoardingBodyState();
-}
-
-class _OnBoardingBodyState extends State<OnBoardingBody> {
-  PageController controller = PageController(
-    initialPage: 0,
-    // viewportFraction: 1.0,
-  );
-
-  @override
-  void initState() {
-    controller.initialPage;
-    super.initState();
-  }
-
-  int currentPage = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: PageView.builder(
-      itemCount: onboardingData(context).length,
-      controller: controller,
-      onPageChanged: (value) {
-        currentPage = value;
-        setState(() {});
-      },
-      itemBuilder: (context, index) {
-        return OnBoardingPageViewItem(
-          list: onboardingData(context),
-          index: index,
-          currentPage: currentPage,
-        );
-      },
-    ));
+    return const Column();
   }
 }
