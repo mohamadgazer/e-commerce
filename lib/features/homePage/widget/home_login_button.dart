a1b55165a3c4e4b9037009701710d0878a8c70ae
mohamadgazer <mohamadgazer@yahoo.com>
2024-01-29 22:39:42 +0200

    - add gester dedector to  HomeLoginButton

--- lib/features/homePage/widget/home_login_button.dart
+++ lib/features/homePage/widget/home_login_button.dart
@@ -5,23 +5,28 @@ import 'package:installment/core/theming/app_color.dart';
 class HomeLoginButton extends StatelessWidget {
   final String text;
   final String icon;
+  final void Function()? onTap;
   const HomeLoginButton({
     super.key,
     required this.text,
     required this.icon,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          borderRadius:
-              BorderRadius.all(Radius.circular(PaddingHelper.borderReduoc)),
-          border: Border.all(color: AppColorManger.greyDeep)),
-      child: ListTile(
-        title: Text(text),
-        leading: Image.asset(icon),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius:
+                BorderRadius.all(Radius.circular(PaddingHelper.borderReduoc)),
+            border: Border.all(color: AppColorManger.greyDeep)),
+        child: ListTile(
+          title: Text(text),
+          leading: Image.asset(icon),
+        ),
       ),
     );
   }
