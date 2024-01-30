a4230964ca10a93495c03d6f6ab67b3e0b3e2d11
mohamadgazer <mohamadgazer@yahoo.com>
2024-01-30 23:10:03 +0200

    - start BottomSignUpWidgets.

---
+++ lib/features/login/widget/bottom_sign_up_widgets.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:installment/core/app_text_style_manger.dart';
+import 'package:installment/core/helper/context_helper.dart';
+import 'package:installment/features/login/view/signup_view.dart';
+import 'package:installment/generated/l10n.dart';
+
+class BottomSignUpWidgets extends StatelessWidget {
+  const BottomSignUpWidgets({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        MaterialButton(
+          onPressed: () {},
+          child: Text(
+            S.of(context).forget_password,
+            style: AppTextStyleManger.s16BookGreyDeeperr,
+          ),
+        ),
+        MaterialButton(
+          onPressed: () {
+            context.pushReplacementNamed(SignUpView.id);
+          },
+          child: Text(
+            S.of(context).sign_up,
+            style: AppTextStyleManger.s16Booksecondary,
+          ),
+        ),
+      ],
+    );
+  }
+}
