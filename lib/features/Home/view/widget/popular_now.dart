128a5d8f5dcd522babfe32a0f31c8bc0534e2f22
mohamadgazer <mohamadgazer@yahoo.com>
2024-02-05 03:02:18 +0200

    - refactor popular now widget.

--- lib/features/Home/view/widget/popular_now.dart
+++ lib/features/Home/view/widget/popular_now.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:installment/core/app_text_style_manger.dart';
 import 'package:installment/core/padding_helper.dart';
-import 'package:installment/core/theming/app_color.dart';
 import 'package:installment/generated/l10n.dart';
+
+import 'popular_container.dart';
 
 class PopularNow extends StatefulWidget {
   const PopularNow({
@@ -42,45 +42,8 @@ class _PopularNowState extends State<PopularNow> {
           style: AppTextStyleManger.s21BlackBlack,
         ),
         SizedBox(height: PH.h26),
-        Container(
-          key: containerKey,
-          width: double.infinity,
-          height: 192.h,
-          decoration: BoxDecoration(
-            color: AppColorManger.main,
-            borderRadius: BorderRadius.circular(PH.borderReduoc32),
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(PH.all24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: containerWidth / 2,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        S.of(context).home_popular,
-                        style: AppTextStyleManger.s21Blackwhite,
-                      ),
-                      SizedBox(height: PH.h18),
-                      Text(
-                        S.of(context).hughlan_Workspaces,
-                        style: AppTextStyleManger.s12BookgreyDeeper,
-                      ),
-                      SizedBox(height: PH.h18),
-                      Text(
-                        "data",
-                        style: AppTextStyleManger.s21Blackwhite,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )
+        PopularContainer(
+            containerKey: containerKey, containerWidth: containerWidth),
       ],
     );
   }
