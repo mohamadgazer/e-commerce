d6c8ab045273f175f6be042240940b47e905542e
mohamadgazer <mohamadgazer@yahoo.com>
2024-02-10 06:02:16 +0200

    - refactor ViewItemBody.

--- lib/features/view_item/widget/view_item_body.dart
+++ lib/features/view_item/widget/view_item_body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:installment/core/padding_helper.dart';
+import 'package:installment/core/theming/app_color.dart';
 import 'package:installment/features/auth/widget/body_template.dart';
-
-import 'view_item_head.dart';
+import 'package:installment/features/view_item/widget/view_item_head.dart';
 
 class ViewItemBody extends StatelessWidget {
   const ViewItemBody({
@@ -10,10 +11,38 @@ class ViewItemBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BodyTemplate(
+    return const Column(
       children: [
-        ViewItemHead(),
+        ViewITemTopTemplate(
+          children: [
+            ViewItemHead(),
+          ],
+        ),
+        BodyTemplate(
+          children: [],
+        ),
       ],
+    );
+  }
+}
+
+class ViewITemTopTemplate extends StatelessWidget {
+  final List<Widget> children;
+  const ViewITemTopTemplate({
+    super.key,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColorManger.whiteShade,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: PH.w24, vertical: PH.h40),
+        child: Column(
+          children: children,
+        ),
+      ),
     );
   }
 }
