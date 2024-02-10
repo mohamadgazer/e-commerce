cb0ad2c6b079ad5cbba9246b8624e1c3a588aaf7
mohamadgazer <mohamadgazer@yahoo.com>
2024-02-10 06:02:52 +0200

    - set ViewITemTopTemplate widget.

---
+++ lib/features/view_item/widget/view_i_tem_top_template.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:installment/core/padding_helper.dart';
+import 'package:installment/core/theming/app_color.dart';
+
+class ViewITemTopTemplate extends StatelessWidget {
+  final List<Widget> children;
+  const ViewITemTopTemplate({
+    super.key,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColorManger.whiteShade,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: PH.w24, vertical: PH.h40),
+        child: Column(
+          children: children,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/view_item/widget/view_item_body.dart
+++ lib/features/view_item/widget/view_item_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:installment/core/padding_helper.dart';
-import 'package:installment/core/theming/app_color.dart';
 import 'package:installment/features/auth/widget/body_template.dart';
 import 'package:installment/features/view_item/widget/view_item_head.dart';
+
+import 'view_i_tem_top_template.dart';
 
 class ViewItemBody extends StatelessWidget {
   const ViewItemBody({
@@ -22,27 +22,6 @@ class ViewItemBody extends StatelessWidget {
           children: [],
         ),
       ],
-    );
-  }
-}
-
-class ViewITemTopTemplate extends StatelessWidget {
-  final List<Widget> children;
-  const ViewITemTopTemplate({
-    super.key,
-    required this.children,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: AppColorManger.whiteShade,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: PH.w24, vertical: PH.h40),
-        child: Column(
-          children: children,
-        ),
-      ),
     );
   }
 }
