820439703ed28720173985d3a0fdae2c55f80065
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-10 03:22:44 +0200

    - add native Splash view.

--- lib/core/routing/routers.dart
+++ lib/core/routing/routers.dart
@@ -2,14 +2,13 @@ import 'package:installment/features/Home/view/home.dart';
 import 'package:installment/features/Setting/view/setting_view.dart';
 import 'package:installment/features/auth/view/login_view.dart';
 import 'package:installment/features/auth/view/signup_view.dart';
-import 'package:installment/features/onboarding/view/onboarding_view.dart';
 import 'package:installment/features/view_item/view/view_item_view.dart';
 
 class Routes {
   // setting
   static const String setting = SettingPageView.id;
   // on bording
-  static const String onBoarding = OnBoardingView.id;
+  static const String onBoarding = "/";
   // auth
   static const String login = LoginView.id;
   static const String signup = SignUpView.id;
